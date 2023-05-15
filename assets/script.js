63c4e6c4e0b873d9ca9ca4e1f33ea8970c21b908
David Gaspard <dgaspard7777@gmail.com>
2023-05-15 13:42:10 -0500

    added Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var APIKey = "14e4aba3cf6f2dc5536be3db0b7e6b7f";
 
 var searchButton = document.querySelector("#searchButton")
-
+var currentLocation= ""
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
     var cityName = document.querySelector("#city").value
@@ -9,7 +9,7 @@ searchButton.addEventListener("click", function(event) {
 })
  
 function geoLocation (city) {
-    var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey
+    var locationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey
     fetch(locationUrl)
     .then((res)=>{
         return res.json()
@@ -18,22 +18,34 @@ function geoLocation (city) {
         console.log(data)
         var currentlat = data[0].lat
         console.log(currentlat)
+        currentLocation = data[0].name
         var currentlon = data[0].lon
         getWeather(currentlat, currentlon) 
     })
 }
 
 function getWeather (lat, lon) {
-    var queryUrl = "https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&appid=" + APIKey
+    var queryUrl = "https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&units=imperial"+"&appid=" + APIKey 
     fetch(queryUrl)
     .then((res)=> {
         return res.json()
     })
     .then((data)=>{
         console.log(data)
-        
+        var dateTime = dayjs.unix(data.current.dt).format('MM-DD-YYYY')
+        var currentIcon = data.current.weather[0].icon
+        var currentWeather= document.querySelector("#currentWeather")
+        var currentTemp = data.current.temp
+        var currentHumidity = data.current.humidity
+        var currentWind = data.current.wind_speed
+       currentWeather.innerHTML = ` <p>${currentLocation} <img src="https://openweathermap.org/img/wn/${currentIcon}@2x.png"/></p> 
+       <p>${dateTime}</P>
+       <p>Current Temperature=${currentTemp}  Humidity=${currentHumidity}  Wind Speed=${currentWind} </p>
+        `
     })
 }
+
+
 
 
 
